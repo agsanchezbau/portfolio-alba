438fc346174ca2bc3e717fc4ed6ac5af1c1db3f3
Alba G <agsan2908@hotmail.com>
2017-04-24 12:46:35 +0200

    cambios gh pages

--- src/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	function heightDetect(){
-		$('.section-bg').css('height', $(window).height());
-	};
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
-});
